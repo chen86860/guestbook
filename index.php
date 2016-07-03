e136825ad8b6996b390b60197c4979797c665e2b
星星 <c@q.com>
2016-07-03 23:45:21 +0800

    hey~I miss you so much

--- index.php
+++ index.php
@@ -13,197 +13,13 @@ if (isset($_COOKIE['commnet_guest_status']) && $_COOKIE['commnet_guest_status'] 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-            font-family: "Open Sans", "lucida grande", "Segoe UI", arial, verdana, "lucida sans unicode", tahoma, sans-serif;
-        }
-
-        .warp {
-            margin: 0 auto;
-            width: 520px;
-        }
-
-        .comment_body {
-            width: 500px;
-            /*background-color: #f9f9f9;*/
-            /*border: 1px solid #e2e2e2;*/
-            margin: 6px 0 6px;
-            padding: 3px;
-            border-radius: 4px;
-            clear: both;
-            display: inline-block;
-        }
-
-        .comment_meta {
-            width: 80px;
-            float: left;
-            margin: 3px;
-        }
-
-        .comment_content {
-            width: 390px;
-            padding: 10px;
-            position: relative;
-            float: right;
-            border: 1px solid #dcdcdc;
-            background-color: #f9f9f9;
-            border-radius: 3px;
-        }
-
-        .header_box > img {
-            width: 54px;
-            text-align: center;
-            margin: 0 auto;
-            display: block;
-        }
-
-        .comment_text {
-            clear: both;
-            position: relative;
-            color: #4e4e4e;
-            font-size: 14px;
-
-        }
-
-        .comment_info {
-            position: relative;
-            bottom: 0;
-            left: 0;
-            color: #c6c6c6;
-            font-size: 13px;
-            padding: 10px 0 0 0;
-        }
-
-        .nickname_show {
-            font-size: 12px;
-            text-align: center;
-            color: #585858;
-        }
-
-        .dot1, .dot2 {
-            display: inline-block;
-            border: 6px solid #dcdcdc;
-            width: 1px;
-            border-top: 6px solid transparent;
-            border-bottom: 6px solid transparent;
-            border-left: 6px solid transparent;
-            position: relative;
-            left: -23px;
-        }
-
-        .dot2 {
-            left: -34px;
-            border: 6px solid #f9f9f9;
-            width: 1px;
-            border-top: 6px solid transparent;
-            border-bottom: 6px solid transparent;
-            border-left: 6px solid transparent;
-            position: relative;
-        }
-
-        .dot {
-            display: inline-block;
-            /* height: 0; */
-            position: absolute;
-        }
-
-        .comment_succeed, .comment_faild {
-            width: 94%;
-            height: auto;
-            text-align: center;
-            font-size: 14px;
-            margin: 0 auto
-        }
-
-        .comment_succeed > p, .comment_faild > p {
-            margin: 5px;
-            padding: 0;
-        }
-
-        .comment_succeed {
-            background-color: #def8e2;
-            color: #637564;
-            border: 1px solid #9be49f;
-
-        }
-
-        .comment_faild {
-            background-color: #fdbfbf;
-            color: #832f2f;
-            border: 2px solid #ff7c7c;
-        }
-
-        .form_post {
-            width: 94%;
-            margin: 0 auto;
-            height: 186px;
-            background-color: #f5f5f5;
-            /* height: 300px; */
-            margin-top: 10px;
-            border: 1px solid #dedede;
-            border-radius: 3px;
-        }
-
-        .form_content {
-            width: 100%;
-            margin: 0 auto;
-        }
-
-        .form_left {
-            width: 225px;
-            float: left;
-            margin-left: 10px;
-        }
-
-        .form_left > input {
-            width: 200px;
-            height: 20px;
-            margin: 10px 0 0 0;
-            border: 1px solid #c3c3c3;
-            border-radius: 3px;
-            padding: 2px 5px 2px;
-        }
-
-        .form_right {
-            width: 238px;
-            float: left;
-        }
-
-        .form_right > textarea {
-            width: 222px;
-            border: 1px solid #c3c3c3;
-            border-radius: 3px;
-            height: 88px;
-            margin-top: 10px;
-            /* background-color: white; */
-            resize: none;
-            padding: 4px 6px 4px;
-        }
-
-        .form_right > p > input[type=submit] {
-            position: relative;
-            width: 90px;
-            right: 3px;
-            height: 29px;
-            background-color: #2196F3;
-            border: 1px solid #03A9F4;
-            color: #fff;
-            border-radius: 4px;
-            font-family: "Open Sans", "lucida grande", "Segoe UI", arial, verdana, "lucida sans unicode", tahoma, sans-serif;
-            cursor: pointer;
-
-        }
-
-        .btn_sub {
-            text-align: right;
-
-        }
-    </style>
+    <title>留言墙</title>
+    <link href="css/index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+<header>
+    <p>留言墙</p>
+</header>
 <div class="warp">
     <?php
     /**
@@ -212,11 +28,42 @@ if (isset($_COOKIE['commnet_guest_status']) && $_COOKIE['commnet_guest_status'] 
      * Date: 16/7/3
      * Time: 12:25
      */
+
+
+    //site
+    //PHP的正则匹配要求两端对称！
+    // 可以用~ @ # 来标志开头和结尾
+    $patten_page = '/^\d{0,3}$/';
+    if (isset($_GET['page'])) {
+        if (preg_match($patten_page, $_GET['page'])) {
+            $page = intval($_GET['page']);
+        } else {
+            $page = 1;
+        }
+
+    } else {
+        $page = 1;
+    }
+
+    $num = 10;         //每页显示10条数据
     include 'conn.php';
+    //选择要操作的数据库
+    /** @var 选择行数 $sql_count_row */
+    $sql_count_row = "select * from comment";
+    $total = mysqli_num_rows(mysqli_query($link, $sql_count_row)); //查询数据的总数total
+    $pagenum = ceil($total / $num);      //获得总页数 pagenum
+    //假如传入的页数参数apge 大于总页数 pagenum，则显示错误信息
+    if ($page > $pagenum || $page == 0) {
+//        echo "error : Can Not Found The page .";exit;
+        $page = 1;
+
+    }
+    $offset = ($page - 1) * $num;         //获取limit的第一个参数的值 offset ，假如第一页则为(1-1)*10=0,第二页为(2-1)*10=10。             (传入的页数-1) * 每页的数据 得到limit第一个参数的值
 
     $sql_check_all_comment = <<<mia
-select id,nickname,comment_content,time,region_city,header from comment order by id
+select id,nickname,comment_content,time,region_city,header from comment order by id limit $offset,$num 
 mia;
+
 
     $result = mysqli_query($link, $sql_check_all_comment);
     $res = array();
@@ -238,6 +85,46 @@ mia;
     }
     //print_r($res);
     mysqli_close($link);
+
+    //页码打印
+    echo "<div class='page_rows'><p>";
+    $page_priv = intval($page) - 1;
+    $page_bak = intval($page) + 1;
+    $page_end = intval($pagenum);
+
+    //首页
+    if (intval($page) == '1') {
+        echo '<a' . ' disabled="true"><span><<</span></a>';
+    } else {
+        echo '<a href=' . 'index.php?page=1' . '><span><<</span></a>';
+    }
+    //前一页
+    if (intval($page) == '1') {
+        echo '<a' . ' disabled="true"><span><</span></a>';
+    } else {
+        echo '<a href=' . 'index.php?page=' . $page_priv . '><</a>';
+    }
+
+    //打印页面
+    for ($i = 1; $i <= $pagenum; $i++) {
+        $show = ($i != $page) ? "<a href='index.php?page=" . $i . "'>$i</a>" : "<span class='page_selectd'>$i</span>";
+        echo $show . " ";
+    }
+
+    //后一页
+    if ($page == $page_end) {
+        echo '<a' . ' disabled="true"><span>></span></a>';
+    } else {
+        echo "<a href=" . "index.php?page=" . $page_bak . ">></a>";
+    }
+
+    //尾页
+    if ($page == $page_end) {
+        echo "<a " . "'><span>>></span></a></p></div>";
+    } else {
+        echo "<a href='index.php?page=" . $page_end . "'><span>>></span></a></p></div>";
+    }
+
     ?>
     <?php
     if ($comment_status == '1') {
@@ -264,20 +151,11 @@ mia;
             </div>
             <div class="form_right">
                 <textarea required placeholder="写下你的评论..." name="comment"></textarea>
-                <p class="btn_sub"><input type="submit" name="submit" value="留个足迹"></div>
-            </p>
+                <p class="btn_sub"><input type="submit" name="submit" value="留个足迹">
+                </p></div>
+
         </div>
     </form>
-    <!--    --><?php
-    //    if (isset($_COOKIE['commnet_guest_status']) && $_COOKIE['commnet_guest_status'] == '1') {
-    ////        $_COOKIE['commnet_guest_status'] = '';
-    //        setcookie('commnet_guest_status', '1',time()-1);
-    //        echo "<p>评论成功!^_^</p>";
-    //    } elseif (isset($_COOKIE['commnet_guest_status']) && $_COOKIE['commnet_guest_status'] == '0') {
-    //        $_COOKIE['commnet_guest_status'] = '';
-    //        echo "<p>评论失败，请检查输入:)</p>";
-    //    }
-    //    ?>
 </div>
 </body>
 </html>
